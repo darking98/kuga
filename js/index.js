b22cf2ba59fecb48343429ac90b624874d8351fa
Diego Rodriguez <diego.gabrielrodriguez@hotmail.com>
2021-06-16 22:38:17 -0300

    making new updates for hero and navbar

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 const header = document.getElementById('header');
 const headerLogo = document.getElementById('logo');
 
-window.addEventListener('scroll', () => {
+/*window.addEventListener('scroll', () => {
     if(window.scrollY >= 75){
         header.style.background = 'black';
         header.style.opacity = '0.7';
@@ -15,3 +15,31 @@ window.addEventListener('scroll', () => {
     }
     
 })
+*/
+var countDownDate = new Date("July 28, 2021 20:00:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  document.getElementById("timer").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("timer").innerHTML = "EXPIRED";
+  }
+}, 1000);
---
+++ js/twitterApi.js
@@ -0,0 +1,4 @@
+const apiKey = 'eykhxFHf8fGgUEjKy9D6rlTQP';
+const apiSecret = 'aWRJKRPHuOmVWYnCmMl5XaCfyDcR2RZe5tQf9xyMhz9pWUrd9h';
+const accesToken = '1141034083394510852-Nz2tzTLxVFwUUvubxgT2dxMil4es2O';
+const secretToken = 'g487luobxSe67VgGI5ghwJ7CX79N8C9CD8rADsGZtSk0K';
